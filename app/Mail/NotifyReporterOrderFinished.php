5abf4fb73e05ac0b48b29a821a92646e9a5e01b3
frashsym <rfqfrashsym@gmail.com>
2025-08-11 15:08:32 +0700

    pake queue

--- app/Mail/NotifyReporterOrderFinished.php
+++ app/Mail/NotifyReporterOrderFinished.php
@@ -8,8 +8,9 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NotifyReporterOrderFinished extends Mailable
+class NotifyReporterOrderFinished extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
